eb32daf25d33ed1111f566facd3093c241e3cefb
AyeshaTayyiba <36958830+AyeshaTayyiba@users.noreply.github.com>
2022-02-05 18:01:52 -0500

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header.js'
 import Search from './components/Search/Search.js'
 import Map from './components/Map/Map.js'
+import NavBar from './components/NavBar.js'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Search />
       <Map />
+      <NavBar />
     </div>
   );
 }
