af681242378c287ff3f38b6bd910463dabc6b5b1
AyeshaTayyiba <36958830+AyeshaTayyiba@users.noreply.github.com>
2022-02-05 18:42:20 -0500

    added bottom NavBar!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,29 +8,6 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import Paper from '@mui/material/Paper';
 
-/*
-export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
-
-  return (
-    <Box sx={{ width: 500}}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
-  );
-}
-*/
-
-
 export default function FixedBottomNavigation() {
   const [value, setValue] = React.useState(0);
   const ref = React.useRef(null);
