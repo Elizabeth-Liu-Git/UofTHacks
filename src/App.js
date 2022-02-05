522c94aa02f2c671353c7a1b91a097724ea010a2
Rita-Liu-Git <Rita.Liu.Yuqiao@gmail.com>
2022-02-05 15:05:16 -0500

    google map added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header.js'
 import Search from './components/Search/Search.js'
+import Map from './components/Map/Map.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map/Map.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 43.651070,
+      lng: -79.347015
+    },
+    zoom: 11
+  };
+
+  state = {
+    divcontainer:false,
+  }
+
+  render() {
+    var Handlechange = e => 
+    {
+      this.setState({divcontainer:!this.state.divcontainer});
+    }
+    const x = this.state.divcontainer;
+    return (
+      <div>
+        <button onClick={Handlechange}>{x?'Hide':'Show'}</button>
+        {
+          x && <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyBb-O1yrZ0z3b5rnp8N3XSZU2nQeDydVEk" }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
